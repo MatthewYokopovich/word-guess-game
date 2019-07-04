0d94f985744e3c1710343e2ea2701947efd92c49
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-07-03 19:34:43 -0500

    finalization of JS Logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,103 @@
-var ans = "answer";
+var ansList = [
+  "predator",
+  "hellraiser", 
+  "chucky"
+];
+var ans = "";
+var displayAns = [];
+var guessCheck = false;
 var guesses = [];
 var lettersGuessed = [];
-var fails = 10;
+var livescount = 10;
 var wins = 0;
+var gameWon = false;
+var game = document.getElementById("game");
+var answer = document.getElementById("answer");
+var lives = document.getElementById("lives");
+var resetDoc = document.getElementById("reset");
+var guessDoc = document.getElementById("guess");
 
-for(var i = 0; i<ans.length; i++){
+
+
+
+function reset(){
+  debugger;
+  livescount = 10;
+  ans = ansList[Math.floor(Math.random()*ansList.length)];
+  wins = 0;
+  for(var i = 0; i<ans.length; i++){
     lettersGuessed[i] = false;
+  }
+  for(var i = 0; i<ans.length; i++){
+    displayAns[i] = '_';
+  }
+  guesses.length = 0;
+  game.textContent = "Press any key to start guessing";
+  answer.textContent = "Movie Villain: "+ displayAns;
+  lives.textContent = "Your Guesses: "+ livescount;
+  guessDoc.textContent = "Wrong Guesses: "+ guesses;
 }
 
-game.textContent = "Wins: 0 "
+function youWin(){
+  debugger;
+  guesses.length = 0;
+  displayAns.length = 0;
+  lettersGuessed.length = 0;
+  wins++;
+  ans = ansList[Math.floor(Math.random()*ansList.length)];
+  for(var i = 0; i<ans.length; i++){
+    lettersGuessed[i] = false;
+  }
+  for(var i = 0; i<ans.length; i++){
+    displayAns[i] = '_';
+  }
+  game.textContent = "Wins: " + wins;
+  answer.textContent = "Movie Villain: "+ displayAns;
+  lives.textContent = "Your Guesses: "+ livescount;
+  guessDoc.textContent = "Wrong Guesses: "+ guesses;
+
+}
+
+function isTrue(testTrue){
+  return testTrue===true;
+}
+
+
+resetDoc.addEventListener("click", reset());
+reset();
+
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
+  //ends game when lives hits 0
+if((livescount!==0)&&(!(guesses.includes(event.key)))){
   // Determines which key was pressed.
   var userGuess = event.key.toLowerCase();
-  // Alerts the key the user pressed (userGuess).
-  alert("User guess: " + userGuess);
 
-  guesses.push(userGuess);
+  guessCheck = false;
 
   for(var i = 0; i < ans.length; i++){
       if(ans[i]===userGuess){
         lettersGuessed[i] = true;
+        displayAns[i] = userGuess;
+        guessCheck = true;
       }
+  }
+  if(guessCheck === false){
+      livescount--;
+      guesses.push(userGuess);
   }
 
   game.textContent = "Wins: " + wins;
+  answer.textContent = "Movie Villain: "+ displayAns;
+  lives.textContent = "Your Guesses: "+ livescount;
+  guessDoc.textContent = "Wrong Guesses: "+ guesses;
+
+  if(lettersGuessed.every(isTrue)){
+    youWin();
+  }
+}
+else{
+  game.innerHTML = "<h1>Too bad, Try again</h1><br>"+
+                    "<h1>Click Reset to try again</h1>";
+}
 }
